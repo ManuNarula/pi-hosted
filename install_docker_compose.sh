03aaf37c3d309b3938ad4199a77a9dfd9166be37
Manu Narula <87030457+ManuNarula@users.noreply.github.com>
2022-03-27 18:57:55 -0700

    Minor change

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -5,8 +5,8 @@ fi
 
 sudo apt-get update # Updates system 
 # Requirements: 
-sudo apt-get install -y libffi-dev libssl-dev python3-dev python3 python3-pip | error "Failed to install dependancies needed!"
+sudo apt-get install -y libffi-dev libssl-dev python3-dev python3 python3-pip || error "Failed to install dependancies needed!"
 # Installs via pip
-sudo pip3 install docker-compose | error "Failed to install docker compose via Pip "
+sudo pip3 install docker-compose || error "Failed to install docker compose via Pip "
 echo 'Installation is complete!'
 exit 
